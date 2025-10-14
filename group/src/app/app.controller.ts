71093c8b982b4cb502e97b75d3337f23c1145ea2
Aashish <aashish.khandelwal.systango@gmail.com>
2025-10-14 12:30:02 +0530

    started the dashboard api with fetch-users

--- group/src/app/app.controller.ts
+++ group/src/app/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
 
   @UseGuards(JwtAuthGuard)  
   @Post('/create-group')
-  async createGroup(@Body() createGroupDto : CreateGroupDto , @Headers('authorization') authHeader: string) : Promise<{ mappedUserGroup: { id: string; user_id: string; group_id: string; createdAt: Date; }; groupCreated: { id: string; createdAt: Date; name: string; admin_user: string; admin_name: string; }; }>  {
+  async createGroup(@Body() createGroupDto : CreateGroupDto , @Headers('authorization') authHeader: string) : /*Promise<{ mappedUserGroup: { id: string; user_id: string; group_id: string; createdAt: Date; }; groupCreated: { id: string; createdAt: Date; name: string; admin_user: string; admin_name: string; }; }>*/ Promise<ApiResponse<>>  {
     const token   = authHeader?.split(" ")[1]    
     const {username , sub : id} = this.authService.customDecode(token)
     const groupPayload = {
@@ -22,10 +22,11 @@ export class AppController {
     return {mappedUserGroup , groupCreated}
   }
 
+  // for dashboard
   @UseGuards(JwtAuthGuard)
   @Get('/fetch-group')
-  async fetchGroup(){
+  async fetchGroup(){    
     // code to fetch the groups on userid from mapped user (handle admin user also)
+    // Centralize the return response body accordin to the DTO's
   }
-
 }
--- shared-dtos/src/lib/group/create-group.dto.ts
+++ shared-dtos/src/lib/group/create-group.dto.ts
@@ -4,3 +4,20 @@ export class CreateGroupDto{
     name : string;
     
 }
+export class CreateGroupResponseDto{
+    {
+    "mappedUserGroup": {
+        "id": "b91d52c9-ace5-46b6-a7c7-b351c4147753",
+        "user_id": "8e36ab25-004a-44c6-8dde-84717379fdbe",
+        "group_id": "92eaaa31-7132-4fa8-a100-ea7060b181df",
+        "createdAt": "2025-10-14T06:32:50.605Z"
+    },
+    "groupCreated": {
+        "id": "92eaaa31-7132-4fa8-a100-ea7060b181df",
+        "name": "group2",
+        "admin_user": "8e36ab25-004a-44c6-8dde-84717379fdbe",
+        "admin_name": "test1",
+        "createdAt": "2025-10-14T06:32:50.149Z"
+    }
+}
+}
